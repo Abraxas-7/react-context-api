48762e1defb53d5e8b6a4cc1765f2317f5253296
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-01-19 20:23:40 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import DefaultLayout from "./pages/DefaultLayout";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import PostsPage from "./pages/PostsPage";
+import AddPostsPage from "./pages/AddPostsPage";
+import PostPage from "./pages/PostPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/contact" Component={ContactPage} />
+          <Route path="/about" Component={AboutPage} />
+          <Route path="/posts">
+            <Route index Component={PostsPage} />
+            <Route path="create" Component={AddPostsPage} />
+            <Route path=":id" Component={PostPage} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <section className="container-fluid">
+      <h1>HomePage</h1>
+    </section>
+  );
+}
